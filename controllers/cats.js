510905507a34f2e37c859ace4f1a0fdab485239a
jnlsings <jnlsings@gmail.com>
2020-02-12 16:17:22 -0600

    add get all cats and post new cat

--- controllers/cats.js
+++ controllers/cats.js
@@ -2,4 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Cat = require('../models/Cat');
 
+router.get('/', (req, res) => {
+  Cat.find({}).then(allCats => res.json(allCats));
+});
+
+router.post('/', (req, res) => {
+  Cat.create(req.body).then(cat =>
+    Cat.find({}).then(allCats => res.json(allCats))
+  );
+});
+
 module.exports = router;
